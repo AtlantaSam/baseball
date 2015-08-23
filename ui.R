286ecf1f143a1d32079e30b41a1de14cd137ba72
AtlantaSam <edge33@mac.com>
2015-08-23 14:14:59 -0400

    final

--- ui.R
+++ ui.R
@@ -9,16 +9,17 @@ shinyUI(pageWithSidebar(
   # Sidebar with controls to select the variable to plot stats
   # adding a trend line with Loess Smoothing becaue it looks cool
   sidebarPanel(
-    selectInput("variable", "Variable:",
+    selectInput("variable", "Choose a Baseball Statistic:",
                 list("Runs" = "R",
                      "Hits" = "H", 
                      "Home Runs" = "HR" 
                      )),
-    sliderInput("range", "Range:",
+    sliderInput("range", "Choose a Range of Years from 1950-2014:",
                 min = 1950, max = 2014, sep="",
                 value = c(1950, 2014), step = 1),
-    sliderInput("decimal", "Loess Smoothing Fraction:", 
-                min = 0.05, max = 0.95, value = 0.2, step= 0.05)
+    sliderInput("decimal", "Exampine Trends with Loess Smoothing Fraction:", 
+                min = 0.05, max = 0.95, value = 0.2, step= 0.05),
+    helpText("visit github repo at https://github.com/AtlantaSam/baseball.git")
   ),
   
   # Show the caption and plot of the requested stat
